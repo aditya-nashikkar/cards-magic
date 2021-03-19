fb7d8a5ba4524f158f695809c4a9ad6e03d50a29
Aditya Nashikkar <adityanashikkar94@gmail.com>
2021-03-19 22:40:02 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes, Router} from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardsComponent } from './components/cards/cards.component';
+import { ResultComponent } from './components/result/result.component';
+
+const appRoutes: Routes = [
+  { path: '', component: CardsComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardsComponent,
+    ResultComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/cards/cards.component.ts
@@ -0,0 +1,164 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cards',
+  templateUrl: './cards.component.html',
+  styleUrls: ['./cards.component.css']
+})
+export class CardsComponent implements OnInit {
+
+  cardsPack = [];
+  selectedCards = [];
+  selectedCardsPack = [];
+  firstRow = [];
+  secondRow = [];
+  thirdRow = [];
+  start: number;
+  end: number;
+  userInput: number;
+  static round: number = 0; 
+  disBtn: boolean;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.getCardsPack();
+    this.getTwentyOneRandomCards();
+    this.setSelectedCards();
+    this.distributeInThree()
+  }
+
+  getCardsPack() {
+    for(let i=1; i<=13; i++) {
+      this.cardsPack.push({
+        number: i,
+        type: '&spades;',
+        color: '#252525'
+      });
+    }
+    for(let i=1; i<=13; i++) {
+      this.cardsPack.push({
+        number: i,
+        type: '&hearts;',
+        color: '#e44145'
+      });
+    }
+    for(let i=1; i<=13; i++) {
+      this.cardsPack.push({
+        number: i,
+        type: '&diams;',
+        color: '#e44145'
+      });
+    }
+    for(let i=1; i<=13; i++) {
+      this.cardsPack.push({
+        number: i,
+        type: '&clubs;',
+        color: '#252525'
+      });
+    }
+  }
+
+  getTwentyOneRandomCards() {
+    while(this.selectedCards.length < 21){
+      let r = Math.floor((Math.random()*51) + 1);
+      if(this.selectedCards.indexOf(r) === -1) 
+        this.selectedCards.push(r);
+    }
+  }
+
+  setSelectedCards() {
+    for(let i=0; i<21; i++) {
+      this.selectedCardsPack[i] = this.cardsPack[this.selectedCards[i]];
+    }
+  }
+
+  distributeInThree() {
+    let count = 0;
+    for(let i=0; i<7; i++) {
+      this.firstRow[count] = this.selectedCardsPack[i];
+      count++;
+    }
+    count = 0;
+    for(let i=7; i<14; i++) {
+      this.secondRow[count] = this.selectedCardsPack[i];
+      count++;
+    }
+    count = 0;
+    for(let i=14; i<21; i++) {
+      this.thirdRow[count] = this.selectedCardsPack[i];
+      count++;
+    }
+  }
+
+  splitCards() {
+    let counter1 = 0;
+    let counter2 = 0;
+    let counter3 = 0;
+    for(let i=0; i<21; i++) {
+      if(i%3 == 0) {
+        this.firstRow[counter1] = this.selectedCardsPack[i];
+        counter1++;
+      } else if(i%3 == 1) {
+        this.secondRow[counter2] = this.selectedCardsPack[i];
+        counter2++;
+      } else if(i%3 == 2) {
+        this.thirdRow[counter3] = this.selectedCardsPack[i];
+        counter3++;
+      }      
+    }
+  }
+
+  sendChoice(i: number) {
+    CardsComponent.round = CardsComponent.round + 1;
+    if(CardsComponent.round <= 3) {
+      if(CardsComponent.round <= 3) {
+        this.rearrangeDeck(i);
+        this.splitCards();
+      } 
+
+      if(CardsComponent.round  === 3) {
+        this.disBtn = true; 
+        console.log(this.selectedCardsPack[10]);
+      }
+    }
+  }
+
+  rearrangeDeck(i) {
+    if(i == 1) {
+      for(let i=0; i<7; i++) {
+        this.selectedCardsPack[i] = this.thirdRow[i];
+      }
+      for(let i=7; i<14; i++) {
+        this.selectedCardsPack[i] = this.firstRow[i-7];
+      }
+      for(let i=14; i<21; i++) {
+        this.selectedCardsPack[i] = this.secondRow[i-14];
+      }
+    } else if(i == 2) {
+      for(let i=0; i<7; i++) {
+        this.selectedCardsPack[i] = this.thirdRow[i];
+      }
+      for(let i=7; i<14; i++) {
+        this.selectedCardsPack[i] = this.secondRow[i-7];
+      }
+      for(let i=14; i<21; i++) {
+        this.selectedCardsPack[i] = this.firstRow[i-14];
+      }
+    } else if(i == 3) {
+      for(let i=0; i<7; i++) {
+        this.selectedCardsPack[i] = this.secondRow[i];
+      }
+      for(let i=7; i<14; i++) {
+        this.selectedCardsPack[i] = this.thirdRow[i-7];
+      }
+      for(let i=14; i<21; i++) {
+        this.selectedCardsPack[i] = this.firstRow[i-14];
+      }
+    }
+  }
+
+  showNumber() {
+    console.log(this.selectedCardsPack[10]);
+  }
+}
